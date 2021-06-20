0c8bf86276f6c8a4b02d6a32b096102c738a2578
ishwar chaiser <icr7.dev@outlook.com>
2021-06-20 05:31:38 +0530

    enable decimals calculation 1.check which one is decimal input and which one is integer. 2.provide operations according to input type like (int,int=int) and (int,double=double) etc. 3.provide output for exception case like 3.0 or 4.0 where int is treated as double 4.provide accurate decimal output for int/int operations where result may be decimal like 5/2=2.5 but our code give wrong output like 5/2=2

--- mvcCalculator/src/main/java/com/icr7/mvcCalculator/CalculatorController.java
+++ mvcCalculator/src/main/java/com/icr7/mvcCalculator/CalculatorController.java
@@ -12,73 +12,183 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CalculatorController {
-	
-	int n1=0,n2=0,radio=0,result = 0;
-	String symbol=null;
-	
-	
+	DecimalChecker myNumber = new DecimalChecker();
+	int n1, n2, radio, result1;
+	String status1, status2, symbol;
+	double d1, d2, result2;
+	boolean quotientIsDouble;
+
 	@RequestMapping("/ShowMyLoginPage")
 	public String ShowloginPage() {
 		return "loginPage";
 	}
-	
-	
+
 	@RequestMapping("/login")
 	public ModelAndView ProessLoginPage(HttpServletRequest request, HttpServletResponse response) {
 		ModelAndView mv = new ModelAndView();
 		String name = request.getParameter("username");
 		String pass = request.getParameter("password");
-		 if(name.toLowerCase().equals("ishwar")&&pass.equals("icr7")) {
-			
+		if (name.toLowerCase().equals("ishwar") && pass.equals("icr7")) {
+
 			mv.setViewName("validPage");
 			mv.addObject("username", name);
-			
-		 }
-		 else {
-			 mv.setViewName("errorPage");
-		     mv.addObject("error","invalid username or password"); 
-		 }
-		 return mv;
+
+		} else {
+			mv.setViewName("errorPage");
+			mv.addObject("error", "invalid username or password");
+		}
+		return mv;
 	}
-	
+
 	@RequestMapping("/ShowMyCalPage")
 	public String ShowCalPage() {
-	
+
 		return "calPage";
 	}
-	
-	
-	
+
 	@RequestMapping("/processCal")
-	public ModelAndView ProcessCal(HttpServletRequest req) {
-	
-		ModelAndView mv =new ModelAndView();
-		
-		if(!(req.getParameter("t1").equals(""))){
-			n1=Integer.parseInt(req.getParameter("t1"));
-		}
-		if(!(req.getParameter("t2").equals(""))){
-			n2=Integer.parseInt(req.getParameter("t2"));
-		}
-		if(!(req.getParameter("opt").equals(""))){
-			symbol=req.getParameter("opt");
+	public ModelAndView ProcessCal(@RequestParam("t1") String s1, @RequestParam("t2") String s2,
+			@RequestParam("opt") String symbol) {
+
+		ModelAndView mv = new ModelAndView();
+
+		if (!s1.equals("")) {
+			Boolean check = myNumber.isInteger(s1);
+			if (check) {
+				n1 = (int) (Double.parseDouble(s1)); // (int)parseDouble(s1) used to satisfy test case for 3.0 like
+														// input where
+														// o/p will be true but still treated as double and cause
+														// nameFormatEception
+				status1 = "integer";
+			} else {
+				d1 = (Double.parseDouble(s1));
+				status1 = "double";
+			}
 		}
 
-		
-		if(symbol.equals("+")){ result=n1+n2;radio=0;}
-		else if(symbol.equals("-")){ result=n1-n2;radio=1;}
-		else if(symbol.equals("*")){ result=n1*n2;radio=2;}
-		else if(symbol.equals("/")){ result=n1/n2;radio=3;}
+		if (!s2.equals("")) {
+			Boolean check = myNumber.isInteger(s2);
+			if (check) {
 
-		    
-			 mv.addObject("num1",n1);
-			 mv.addObject("num2",n2);
-			 mv.addObject("opt",symbol);
-			 mv.addObject("symbolNum",radio);
-			 mv.addObject("output",result);
-		     mv.setViewName("resultPage");
-			
-	    return mv;
+				n2 = (int) (Double.parseDouble(s2));
+				status2 = "integer";
+			} else {
+				d2 = (Double.parseDouble(s2));
+				status2 = "double";
+
+			}
+
+		}
+
+		// int,int
+		if (status1.equals("integer") && status2.equals("integer")) {
+			if (symbol.equals("+")) {
+				result1 = n1 + n2;
+				radio = 0;
+			} else if (symbol.equals("-")) {
+				result1 = n1 - n2;
+				radio = 1;
+			} else if (symbol.equals("*")) {
+				result1 = n1 * n2;
+				radio = 2;
+			} else if (symbol.equals("/")) {
+				if (n1 % n2 == 0) {
+					quotientIsDouble = false;
+					result1 = n1 / n2;
+				} else {
+					quotientIsDouble = true;
+					// for providing accurate decimal result in case both input are int
+				}
+
+				radio = 3;
+			}
+
+			mv.addObject("num1", n1);
+			mv.addObject("num2", n2);
+			mv.addObject("opt", symbol);
+			mv.addObject("symbolNum", radio);
+			if (symbol.equals("/") && quotientIsDouble)// for providing accurate decimal result in case both input are
+														// int
+			{
+				mv.addObject("output", (double) n1 / (double) n2);
+			} else
+				mv.addObject("output", result1);
+			mv.setViewName("resultPage");
+
+		}
+		// int,double
+		else if (status1.equals("integer") && status2.equals("double")) {
+			if (symbol.equals("+")) {
+				result2 = (int) n1 + d2;
+				radio = 0;
+			} else if (symbol.equals("-")) {
+				result2 = (int) n1 - d2;
+				radio = 1;
+			} else if (symbol.equals("*")) {
+				result2 = (int) n1 * d2;
+				radio = 2;
+			} else if (symbol.equals("/")) {
+				result2 = (int) n1 / d2;
+				radio = 3;
+			}
+
+			mv.addObject("num1", n1);
+			mv.addObject("num2", d2);
+			mv.addObject("opt", symbol);
+			mv.addObject("symbolNum", radio);
+			mv.addObject("output", result2);
+			mv.setViewName("resultPage");
+
+		}
+		// double,int
+		else if (status1.equals("double") && status2.equals("integer")) {
+			if (symbol.equals("+")) {
+				result2 = d1 + (double) n2;
+				radio = 0;
+			} else if (symbol.equals("-")) {
+				result2 = d1 - (double) n2;
+				radio = 1;
+			} else if (symbol.equals("*")) {
+				result2 = d1 * (double) n2;
+				radio = 2;
+			} else if (symbol.equals("/")) {
+				result2 = d1 / (double) n2;
+				radio = 3;
+			}
+
+			mv.addObject("num1", d1);
+			mv.addObject("num2", n2);
+			mv.addObject("opt", symbol);
+			mv.addObject("symbolNum", radio);
+			mv.addObject("output", result2);
+			mv.setViewName("resultPage");
+
+		}
+		// double,double
+		else if (status1.equals("double") && status2.equals("double")) {
+			if (symbol.equals("+")) {
+				result2 = d1 + d2;
+				radio = 0;
+			} else if (symbol.equals("-")) {
+				result2 = d1 - d2;
+				radio = 1;
+			} else if (symbol.equals("*")) {
+				result2 = d1 * d2;
+				radio = 2;
+			} else if (symbol.equals("/")) {
+				result2 = d1 / d2;
+				radio = 3;
+			}
+
+			mv.addObject("num1", d1);
+			mv.addObject("num2", d2);
+			mv.addObject("opt", symbol);
+			mv.addObject("symbolNum", radio);
+			mv.addObject("output", result2);
+			mv.setViewName("resultPage");
+
+		}
+
+		return mv;
 	}
 }
-	
